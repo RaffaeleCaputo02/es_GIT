2ab946c0a073299d3131a6ce6fff8577150af9f7
rcaputo95 <rcaputo95@gmail.com>
2018-12-18 15:26:43 +0100

    ho inserito un messaggio di saluto

---
+++ es git hub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_git_hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buongiorno");
+            Console.ReadLine();
+        }
+    }
+}
